66f0667f767594561c2e07a36a2ce9767b15f983
Dmitry Sychev <dimtrisythev@gmail.com>
2022-11-01 13:07:20 +0600

    add block "what we do"
    fix:code format in script
    fix:adaptation the card grid

--- lab1-create-web-site/scripts/script.js
+++ lab1-create-web-site/scripts/script.js
@@ -1,50 +1,52 @@
-const menu=document.querySelector("#menu");
-const closeImg=document.querySelector(".logo_image--burger__close");
-const openImg=document.querySelector(".logo_image--burger__open");
-const button=document.querySelector(".logo_image--burger")
-let isOpen=false;
-const navbar=document.querySelector(".navbar");
-let oldScrollY=0;
+const menu = document.querySelector("#menu");
+const closeImg = document.querySelector(".logo_image--burger__close");
+const openImg = document.querySelector(".logo_image--burger__open");
+const button = document.querySelector(".logo_image--burger");
+const navbar = document.querySelector(".navbar");
 
+let isOpen = false;
+let oldScrollY = 0;
 
-const openMenu=()=>{
+const openMenu = () => {
+  menu.style.opacity = 1;
+  menu.style.top = 0;
 
-    menu.style.opacity=1;
-    menu.style.top=0;
-    closeImg.style.display="block";
-    openImg.style.display="none";
-    document.body.style.overflow="hidden"
-    isOpen=true;
-}
+  closeImg.style.display = "block";
+  openImg.style.display = "none";
+  document.body.style.overflowY = "hidden";
 
-const closeMenu=()=>{
-    menu.style.opacity=1;
-    menu.style.top=" -100vh";
-    closeImg.style.display="none";
-    openImg.style.display="block";
-    document.body.style.overflow="scroll"
-    isOpen=false;
-}
+  isOpen = true;
+};
 
-const menuEventClick=()=>{
-    if(isOpen){
-        closeMenu(); 
-    }
-    else  openMenu();
-}
+const closeMenu = () => {
+  menu.style.opacity = 1;
+  menu.style.top = " -100vh";
 
-const scrollWin =()=> {
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    let dY = scrolled - oldScrollY;
-    
-    if ( dY < 0 ){
-      navbar.style.top=0;
-    } else {
-      navbar.style.top="-100px";
-    }
-    
-    oldScrollY = scrolled;
-  
+  closeImg.style.display = "none";
+  openImg.style.display = "block";
+  document.body.style.overflowY = "scroll";
+  isOpen = false;
+};
+
+const menuEventClick = () => {
+  if (isOpen) {
+    closeMenu();
+  } else {
+    openMenu();
   }
-button.addEventListener("click",menuEventClick);
-window.addEventListener("scroll",scrollWin)
+};
+
+const scrollWin = () => {
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  let dY = scrolled - oldScrollY;
+
+  if (dY < 0) {
+    navbar.style.top = 0;
+  } else {
+    navbar.style.top = "-100px";
+  }
+  oldScrollY = scrolled;
+};
+
+button.addEventListener("click", menuEventClick);
+window.addEventListener("scroll", scrollWin);
